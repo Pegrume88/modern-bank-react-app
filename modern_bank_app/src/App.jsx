312a7cdf2e77e2e56ca4b436a0aa36fd92e2da6b
Rhys Pegrume <pegrume88@gmail.com>
2023-11-14 14:21:17 +0100

    imported all components into App.jsx

--- modern_bank_app/src/App.jsx
+++ modern_bank_app/src/App.jsx
@@ -1,5 +1,8 @@
 import styles from "./style";
 
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+
+
 const App = () =>  (
   <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
