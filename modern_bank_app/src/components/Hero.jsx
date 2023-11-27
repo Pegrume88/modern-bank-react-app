60a952b5ee4f32acdbf96027f35c6db359b45de6
Rhys Pegrume <pegrume88@gmail.com>
2023-11-27 14:11:16 +0100

    imported style.js, discount and robot.

--- modern_bank_app/src/components/Hero.jsx
+++ modern_bank_app/src/components/Hero.jsx
@@ -1,9 +1,12 @@
+import styles from '../style'
+import { discount, robot } from '../assets'
 import React from 'react'
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+const Hero = () => (
+  <section id='home' className={``}>
+
+  </section>
+)
+
 
 export default Hero
